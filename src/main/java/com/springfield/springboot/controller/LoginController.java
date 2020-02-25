3fbc41a2ac5ad2200cb3a87b0b2e3a4f45ec5b93
James-byrne-7 <james.byrne.7@ucdconnect.ie>
2020-02-25 21:50:57 +0000

    Added Error Message to login.

--- src/main/java/com/springfield/springboot/controller/LoginController.java
+++ src/main/java/com/springfield/springboot/controller/LoginController.java
@@ -19,7 +19,16 @@ public class LoginController {
     @Autowired StaffRepository staffRepository;
 
     @RequestMapping(value={"/","/login"}, method=RequestMethod.GET)
-    public String showLoginPage(ModelMap model){
+    public String showLoginPage(@RequestParam(value = "error", required = false) String error,
+                                @RequestParam(value = "logout", required = false) String logout, ModelMap model){
+        String errorMessage = null;
+        if(error != null) {
+            errorMessage = "Username or Password is incorrect !!";
+        }
+        if(logout != null) {
+            errorMessage = "You have been successfully logged out !!";
+        }
+        model.addAttribute("errorMessage", errorMessage);
         return "login";
     }
 
@@ -38,8 +47,17 @@ public class LoginController {
             model.addAttribute("privilegeLevel", "staff");
             return "homepage";
         } else {
-            return "login";
+            return "redirect:/login?error=true";
         }
     }
+
+//    @RequestMapping(value="/logout", method = RequestMethod.GET)
+//    public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
+//        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+//        if (auth != null){
+//            new SecurityContextLogoutHandler().logout(request, response, auth);
+//        }
+//        return "redirect:/login?logout=true";
+//    }
 
 }
--- src/main/webapp/WEB-INF/jsp/login.jsp
+++ src/main/webapp/WEB-INF/jsp/login.jsp
@@ -1,19 +1,22 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+		 pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <html>
 	<head>
 		<link rel="stylesheet" href="portal.css">
 		<title>Login: Student Management System</title>
 	</head>
-<body>
-
-	<div class="main" align="center">
-		<p class="title">Sign in</p>
-		<form action="login" method="post">
-			<input class="field" name="username" type="text" placeholder="Username">
-			<input class="field" name="password" type="password" placeholder="Password">
-			<input class="submit" type="submit" value="Sign in">
-			<p><a href="/register" class="register">Register New Student</a></p>
-		</form>
-	</div>
-</body>
+	<body onload='document.login.username.focus();'>
+		<div class="main" align="center">
+			<p class="title">Sign in</p>
+			<c:if test="${not empty errorMessage}"><div style="color:red; font-weight: bold; margin: 0;">${errorMessage}</div></c:if>
+			<form name ="login" action="login" method="post">
+				<input class="field" name="username" type="text" placeholder="Username">
+				<input class="field" name="password" type="password" placeholder="Password">
+				<input class="submit" type="submit" value="Sign in">
+				<p><a href="/register" class="register">Register New Student</a></p>
+			</form>
+		</div>
+	</body>
 
 </html>
