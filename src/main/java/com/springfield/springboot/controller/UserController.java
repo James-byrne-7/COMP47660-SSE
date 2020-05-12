0908bb4af657257ce4125c1713e3d2a9491b7500
James-byrne-7 <james.byrne.7@ucdconnect.ie>
2020-05-12 15:43:39 +0100

    Combine Login & Register into One 'User' Controller.

--- src/main/java/com/springfield/springboot/controller/UserController.java
+++ src/main/java/com/springfield/springboot/controller/UserController.java
@@ -1,12 +1,13 @@
 package com.springfield.springboot.controller;
 
+
 import com.springfield.springboot.model.User;
-import com.springfield.springboot.repository.UserRepository;
 import com.springfield.springboot.service.SecurityService;
 import com.springfield.springboot.service.UserService;
 import com.springfield.springboot.validator.InputValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
@@ -14,31 +15,27 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpSession;
 
 @Controller
-public class RegisterController {
+public class UserController {
     @Autowired
     UserService userService;
     @Autowired
-    InputValidator inputValidator;
-    @Autowired
     private SecurityService securityService;
+    @Autowired
+    private InputValidator inputValidator;
+
+    @RequestMapping({"/", "/welcome", "/home"})
+    public String landingPage(Model model) {
+        return "home";
+    }
 
     @GetMapping(value="/register")
     public String showRegisterPage(/*@RequestParam(value = "error", required = false) String error,*/ ModelMap model, HttpSession session){
-        session.invalidate();
-        String loginMessage = null;
-//        if(error != null) {
-//            loginMessage = "Details are invalid, please try again";
-//        }
-//        model.addAttribute("errorMessage", loginMessage);
         model.addAttribute("newUser", new User());
         return "register";
     }
 
     @PostMapping("/register")
     public String registration(@ModelAttribute("newUser") User newUser, BindingResult bindingResult, HttpSession session) {
-        Object userID = session.getAttribute("CURRENT_USER");
-        if (userID != null)
-            session.invalidate();
         inputValidator.validate(newUser, bindingResult);
         if (bindingResult.hasErrors()) {
             System.out.println(bindingResult.getAllErrors().get(0).toString());
@@ -50,6 +47,23 @@ public class RegisterController {
         return "redirect:/home";
     }
 
+    @GetMapping(value="/login")
+    public String showLoginPage(Model model, String error, String logout){
+        if(error != null) {
+            model.addAttribute("loginMessage","An Error has occurred");
+        }
+        if(logout != null) {
+            model.addAttribute("loginMessage","You have been successfully logged out !!");
+        }
+        return "login";
+    }
 
+//    @RequestMapping(value="/dropout")
+//    public String deleteUser(ModelMap model, HttpServletRequest request) throws UserNotFoundException{
+//        Long userID = (Long) request.getSession().getAttribute("CURRENT_USER");
+//        User user = userRepository.findById(userID).orElseThrow(() -> new UserNotFoundException(userID));
+//        userRepository.delete(user);
+//        return "redirect:logout";
+//    }
 
 }
