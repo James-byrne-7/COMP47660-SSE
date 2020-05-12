8cb8c963678e953398fe4817cf576e3033ec50d8
James-byrne-7 <james.byrne.7@ucdconnect.ie>
2020-05-12 15:46:35 +0100

    Remove Custom Exception Classes

--- src/main/java/com/springfield/springboot/exception/ModuleNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.springfield.springboot.exception;
-
-public class ModuleNotFoundException extends Exception {
-    public ModuleNotFoundException(long module_id) {
-        super(String.format("Book is not found with id : " + module_id));
-    }
-}
