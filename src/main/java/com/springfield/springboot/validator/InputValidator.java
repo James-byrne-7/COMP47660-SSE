c20b6af079cd7fef4b824098c4b234e02bc8a532
James-byrne-7 <james.byrne.7@ucdconnect.ie>
2020-05-14 11:06:05 +0100

    Refactor

--- src/main/java/com/springfield/springboot/validator/InputValidator.java
+++ src/main/java/com/springfield/springboot/validator/InputValidator.java
@@ -1,6 +1,5 @@
 package com.springfield.springboot.validator;
 
-import com.springfield.springboot.repository.UserRepository;
 import com.springfield.springboot.model.User;
 import com.springfield.springboot.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,11 +7,15 @@ import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
+import org.apache.commons.validator.routines.EmailValidator;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 @Component
 public class InputValidator implements Validator {
+    final static String PASSWORD_REQUIREMENTS = "((?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[$@$!%*#?&^]).{8,32})";
+    final static String USERNAME_REQUIREMENTS = "^(\\w{1,32}$)";
+
     @Autowired
     private UserService userService;
 
@@ -25,11 +28,11 @@ public class InputValidator implements Validator {
     public void validate(Object obj, Errors errors) {
         User user = (User) obj;
 
-        if (!isUserValid(user.getUsername()))
+        if (doesNotMeet(user.getUsername(), USERNAME_REQUIREMENTS))
             errors.rejectValue("username", "register.username.invalid");
         if (userService.findByUsername(user.getUsername()) != null)
             errors.rejectValue("username", "register.username.exists");
-        if (!isStrong(user.getPassword()))
+        if (doesNotMeet(user.getPassword(), PASSWORD_REQUIREMENTS))
             errors.rejectValue("password", "register.password.invalid");
         if (user.getNationality() == null)
             errors.rejectValue("nationality", "register.required");
@@ -42,12 +45,9 @@ public class InputValidator implements Validator {
         if (doesNotMeet(password, PASSWORD_REQUIREMENTS))
             errors.rejectValue("password", "register.password.invalid");
     }
-
-    private static boolean isStrong(String password){
-        // Password must be 8-32 characters and contain at least 1 uppercase, lowercase and special character.
-        final String PASSWORD_PATTERN = "((?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[$@$!%*#?&^]).{8,32})";
-        Pattern pattern = Pattern.compile(PASSWORD_PATTERN);
-        Matcher matcher = pattern.matcher(password);
-        return matcher.matches();
+    private boolean doesNotMeet(String input, String requirements) {
+        Pattern pattern = Pattern.compile(requirements);
+        Matcher matcher = pattern.matcher(input);
+        return !matcher.matches();
     }
 }
