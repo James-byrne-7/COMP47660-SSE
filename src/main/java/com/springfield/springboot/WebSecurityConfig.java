9f306ae507b23f5ecdaf80518cbca6bdf36304b5
James-byrne-7 <james.byrne.7@ucdconnect.ie>
2020-05-14 16:36:34 +0100

    Replace JSESSION with more secure JWT

--- src/main/java/com/springfield/springboot/WebSecurityConfig.java
+++ src/main/java/com/springfield/springboot/WebSecurityConfig.java
@@ -1,59 +1,87 @@
-    package com.springfield.springboot;
+package com.springfield.springboot;
 
-    import org.springframework.beans.factory.annotation.Autowired;
-    import org.springframework.beans.factory.annotation.Qualifier;
-    import org.springframework.context.annotation.Bean;
-    import org.springframework.context.annotation.Configuration;
-    import org.springframework.security.authentication.AuthenticationManager;
-    import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-    import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-    import org.springframework.security.config.annotation.web.builders.WebSecurity;
-    import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-    import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-    import org.springframework.security.core.userdetails.UserDetailsService;
-    import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import com.springfield.springboot.filter.JWTAuthenticationFilter;
+import com.springfield.springboot.filter.JWTAuthorisationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-    @Configuration
-    @EnableWebSecurity
-    public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-        @Qualifier("simpleUserDetailsService")
-        @Autowired
-        private UserDetailsService userDetailsService;
+import static com.springfield.springboot.filter.SecurityConstants.*;
 
-        @Bean
-        public BCryptPasswordEncoder bCryptPasswordEncoder() {
-            return new BCryptPasswordEncoder();
-        }
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Qualifier("simpleUserDetailsService")
+    @Autowired
+    private UserDetailsService userDetailsService;
 
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http.authorizeRequests()
-                    .antMatchers("/*.css", "/*.js",  "/resources/**", "/register", "/confirm-reset", "/reset-password")
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors()
+            .and()
+                .csrf()
+                    .disable()
+                .requiresChannel()
+                .anyRequest()
+                    .requiresSecure()
+            .and()
+                .authorizeRequests()
+                    .antMatchers("/*.css", "/*.js","/resources/**", "/login","/register", "/forgotpassword", "/confirm-reset")
                         .permitAll()
-                    .anyRequest()
-                        .authenticated()
-                    .and()
-                        .formLogin()
-                        .loginPage("/login")
-                            .permitAll()
-                    .and()
-                        .logout()
-                            .permitAll();
-        }
+                .anyRequest()
+                    .authenticated()
+            .and()
+                .formLogin()
+                .loginPage("/login")
+                    .permitAll()
+            .and()
+                .logout()
+                    .logoutUrl("/logout")
+                    .deleteCookies(COOKIE_NAME)
+                    .permitAll()
+            .and()
+                .addFilter(new JWTAuthenticationFilter(authenticationManager()))
+                .addFilter(new JWTAuthorisationFilter(authenticationManager()))
+                // this disables session creation on Spring Security
+                .sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+}
 
-        @Override
-        public void configure(WebSecurity web) throws Exception {
-            web.ignoring().antMatchers("/forgotpassword", "/*.css", "/*.js");
-            web.ignoring().antMatchers("/confirm-reset");
-        }
-
-        @Bean
-        public AuthenticationManager customAuthenticationManager() throws Exception {
-            return authenticationManager();
-        }
-
-        @Autowired
-        public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-            auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
-        }
+    public WebSecurityConfig(UserDetailsService userDetailsService, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userDetailsService = userDetailsService;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
+
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return authenticationManager();
+    }
+
+    @Autowired
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+    }
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+
+        return source;
+    }
+}
---
+++ src/main/java/com/springfield/springboot/filter/JWTAuthenticationFilter.java
@@ -0,0 +1,73 @@
+package com.springfield.springboot.filter;
+
+import com.auth0.jwt.JWT;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+
+import static com.auth0.jwt.algorithms.Algorithm.HMAC512;
+import static com.springfield.springboot.filter.SecurityConstants.*;
+
+public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    private AuthenticationManager authenticationManager;
+
+    public JWTAuthenticationFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+            throws AuthenticationException {
+        return authenticationManager.authenticate(
+            new UsernamePasswordAuthenticationToken(
+                request.getParameter("username"),
+                request.getParameter("password"),
+                new ArrayList<>())
+        );
+    }
+
+    @Override
+    protected void successfulAuthentication(
+            HttpServletRequest request,HttpServletResponse response,
+            FilterChain chain, Authentication auth)
+            throws IOException, ServletException {
+        String token = JWT.create()
+            .withSubject(((User) auth.getPrincipal()).getUsername())
+            .withExpiresAt(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+            .sign(HMAC512(SECRET.getBytes()));
+        addCookie(token, response);
+
+        new DefaultRedirectStrategy().sendRedirect(request, response, "/welcome");
+    }
+
+    private void addCookie(String token, HttpServletResponse response){
+        Cookie cookie = new Cookie("JWT",token);
+        cookie.setMaxAge(7 * 24 * 60 * 60);
+        cookie.setSecure(true);
+        cookie.setHttpOnly(true);
+        cookie.setPath("/");
+
+        response.addCookie(cookie);
+    }
+}
+
+
+
+
+
+
+
+
